a363dc6878a04f3b13f546bebe21a3b3c0ed74cc
Josep Piguillem <jgpigui@gmail.com>
2024-12-16 20:49:43 +0100

    Initial commit

---
+++ apps/pi-pm-client-e2e/jest.config.ts
@@ -0,0 +1,16 @@
+export default {
+  displayName: 'pi-pm-client-e2e',
+  preset: '../../jest.preset.js',
+  setupFiles: ['<rootDir>/src/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/pi-pm-client-e2e',
+};
---
+++ apps/pi-pm-client/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'pi-pm-client',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/pi-pm-client',
+};
